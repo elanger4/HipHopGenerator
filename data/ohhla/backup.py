648b3829acda304bb0f23e4f8d89491947f17aaa
Jack Fischer <jfische8@binghamton.edu>
2017-03-24 11:30:35 -0400

    Done scraping

--- data/ohhla/backup.py
+++ data/ohhla/backup.py
@@ -5,5 +5,7 @@ import secrets
 db = MongoClient('mongodb://%s:%s@ds139470.mlab.com:39470/hh' % (secrets.user, secrets.password)).hh
 lyric_collection = db.lyric_collection
 
+
 for song in lyric_collection.find():
-  print(song["lyrics"])
+  print(song)
+
