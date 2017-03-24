b3c78d0ba67f5f30271f95fda2dee568e9d6a690
Jack Fischer <jfische8@binghamton.edu>
2017-03-24 10:10:31 -0400

    miner working

---
+++ data/access.py
@@ -0,0 +1,11 @@
+from pymongo import MongoClient
+
+import secrets
+
+db = MongoClient('mongodb://%s:%s@ds139470.mlab.com:39470/hh' % (secrets.user, secrets.password)).hh
+lyric_collection = db.lyric_collection
+
+print("%d songs" % lyric_collection.count())
+
+for song in lyric_collection.find():
+  print(song["lyrics"])
--- data/ohhla/capture.py
+++ data/ohhla/capture.py
@@ -5,23 +5,29 @@ import requests
 import secrets
 
 db = MongoClient('mongodb://%s:%s@ds139470.mlab.com:39470/hh' % (secrets.user, secrets.password)).hh
+lyric_collection = db.lyric_collection
 
 domain = "http://ohhla.com/"
 fav_list_url = domain + "favorite.html"
 soup = BeautifulSoup( requests.get(fav_list_url).text , "html.parser" )
 artist_links = [a["href"] for a in soup.select("a[href^=YFA_]")]
 
+print("ok")
+mongo_buffer = []
 for artist in artist_links[:]:
   artist_soup = BeautifulSoup( requests.get(domain + artist).text, "html.parser" )
   song_links = [a["href"] for a in artist_soup.select("a[href$=txt]")]
 
   for song in song_links[:]:
+    print("%s %s" % (artist, song))
     song_content = requests.get(domain + song).text
     song_soup = BeautifulSoup(song_content, "html.parser")
-    lyrics = song_soup.find("pre").text
-    lyrics = lyrics[lyrics.index("\n\n"):]
+    lyrics = song_soup.find("pre").text if song_soup.find() else song_content
+    if "\n\n" in lyrics: lyrics = lyrics[lyrics.index("\n\n"):]
     lyrics = [l.strip() for l in lyrics.split("\n") if l and not '[' in l]
-    print(lyrics)
 
-
+    mongo_buffer.append({'artist': artist, 'song': song, 'lyrics': "\n".join(lyrics)})
+    if len(mongo_buffer) > 100:
+      print(lyric_collection.insert_many(mongo_buffer))
+      mongo_buffer.clear()
 
