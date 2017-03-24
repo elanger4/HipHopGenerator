8fa124c34505e0a473e301b2f90c37cc9a4bae3e
Jack Fischer <jfische8@binghamton.edu>
2017-03-23 21:39:29 -0400

    brittle proof of concept working for most

--- data/ohhla/capture.py
+++ data/ohhla/capture.py
@@ -1,12 +1,27 @@
 from bs4 import BeautifulSoup
+from pymongo import MongoClient
 import requests
-#miner/futurehendrix
 
+import secrets
+
+db = MongoClient('mongodb://%s:%s@ds139470.mlab.com:39470/hh' % (secrets.user, secrets.password)).hh
 
 domain = "http://ohhla.com/"
 fav_list_url = domain + "favorite.html"
 soup = BeautifulSoup( requests.get(fav_list_url).text , "html.parser" )
-artist_links = [domain + a["href"] for a in soup.select("a[href^=YFA_]")]
-for 
+artist_links = [a["href"] for a in soup.select("a[href^=YFA_]")]
+
+for artist in artist_links[:]:
+  artist_soup = BeautifulSoup( requests.get(domain + artist).text, "html.parser" )
+  song_links = [a["href"] for a in artist_soup.select("a[href$=txt]")]
+
+  for song in song_links[:]:
+    song_content = requests.get(domain + song).text
+    song_soup = BeautifulSoup(song_content, "html.parser")
+    lyrics = song_soup.find("pre").text
+    lyrics = lyrics[lyrics.index("\n\n"):]
+    lyrics = [l.strip() for l in lyrics.split("\n") if l and not '[' in l]
+    print(lyrics)
+
 
 
